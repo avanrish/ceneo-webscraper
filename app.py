f44acacbae418a977bd8381e85c58ddfce4f99a2
Bartosz Lipiński <bartosz.s.lipinski@gmail.com>
2022-03-14 14:08:56 +0100

    redirect and errors work correctly

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, make_response, redirect, render_template, url_for
+from flask import Flask, redirect, render_template, request, url_for
 import json
 
 from functions import fetch_opinions
@@ -10,36 +10,41 @@ def home():
     url_for('static', filename='index.css')
     return render_template('homepage.html')
 
-@app.route('/extract')
+@app.route('/extract', methods=['GET', 'POST'])
 def extract():
-    url_for('static', filename='index.css')
-    return render_template('extract.html')
+    if request.method == "POST":
+        try:
+            id = request.form['product-id']
+            res = fetch_opinions(id)
+            exists = False
+            products = []
+            with open('products.json') as p:
+                products = json.loads(p.read())
+                for product in products:
+                    if product["id"] == id:
+                        exists = True
+                        break
+            if not exists:
+                with open('products.json', 'w') as f:
+                    products.append({"id": id, "title": res["title"]})
+                    f.write(json.dumps(products))
+            with open(f'products/{id}.json', 'w') as item:
+                item.write(json.dumps(res["reviews"]))
+            return redirect(f'/product/{id}')
+        except:
+            return render_template('extract.html', error="Nie znaleziono produktu.")
+    else:
+        return render_template('extract.html')
 
-@app.route('/api/extract/<id>')
-def get_opinions(id):
-    try:
-        res = fetch_opinions(id)
-        exists = False
-        products = []
-        with open('products.json') as p:
-            products = json.loads(p.read())
-            for product in products:
-                if product["id"] == id:
-                    exists = True
-                    break
-        if not exists:
-            with open('products.json', 'w') as f:
-                products.append({"id": id, "title": res["title"]})
-                f.write(json.dumps(products))
-        with open(f'products/{id}.json', 'w') as item:
-            item.write(json.dumps(res["reviews"]))
+@app.route('/products')
+def products():
+    return '<p>Products</p>'
 
-        return redirect(f'/product/{id}')
-    except BaseException as e:
-        return make_response('Wrong ID', 400)
+@app.route('/author')
+def author():
+    return '<p>Author</p>'
 
-
-@app.route('*')
+@app.route('/*')
 def catch_all():
     url_for('static', filename='index.css')
     return render_template('homepage.html')
