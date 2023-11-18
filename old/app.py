d57594ab8e25e2ba13ffd1e926cb4ec803ed0a4e
Bartosz Lipiński <bartosz.s.lipinski@gmail.com>
2023-11-18 12:04:30 +0100

    refactor: cleanup project files and init with poetry

--- old/app.py
+++ old/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    
+
     return render_template('homepage.html')
 
 @app.route('/extract', methods=['GET', 'POST'])
@@ -21,14 +21,14 @@ def extract():
             products = []
             with open(f'products/{id}.json', 'w') as item:
                 item.write(json.dumps(res["reviews"]))
-            with open('products.json') as p:
+            with open('../products.json') as p:
                 products = json.loads(p.read())
                 for product in products:
                     if product["id"] == id:
                         exists = True
                         break
             if not exists:
-                with open('products.json', 'w') as f:
+                with open('../products.json', 'w') as f:
                     del res["reviews"]
                     res["id"] = id
                     products.append(res)
@@ -41,21 +41,21 @@ def extract():
 
 @app.route('/products')
 def products():
-    with open('products.json') as f:
+    with open('../products.json') as f:
         data_array = json.loads(f.read())
-    
+
     return render_template('products.html', prods=data_array)
 
 @app.route('/product/<id>')
 def product(id):
     try:
-        with open('products.json') as f:
+        with open('../products.json') as f:
             all_products = json.loads(f.read())
         product = next(item for item in all_products if item['id'] == id)
         with open(f'products/{id}.json') as f2:
             data=json.loads(f2.read())
         headers=['ID', "Autor", "Rekomendacja", "Ocena", "Potwierdzone zakupem", "Data opinii", "Data zakupu", "Przydatna", "Nieprzydatna", "Treść", "Zalety", "Wady"]
-        
+
         return render_template('product.html', title=product['title'], id=product["id"], data=data, headers=headers)
     except:
         return redirect(url_for('extract'))
@@ -63,7 +63,7 @@ def product(id):
 @app.route('/product/<id>/stats')
 def stats(id):
     try:
-        with open('products.json') as f:
+        with open('../products.json') as f:
             all_products = json.loads(f.read())
         product = next(item for item in all_products if item['id'] == id)
         with open(f'products/{id}.json') as f2:
@@ -76,7 +76,7 @@ def stats(id):
             else:
                 recs['bad'] += 1
             rating[review['rating']] += 1
-        
+
         pie = pieChart(recs)
         bar = barChart(rating)
         return render_template('charts.html', title=product['title'], id=id, pieChart=pie, barChart=bar)
@@ -101,5 +101,5 @@ def author():
 
 @app.route('/*')
 def catch_all():
-    
+
     return render_template('homepage.html')
