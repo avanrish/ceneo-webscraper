2260d974f85f276af1f250521ef95810642351d9
Bartosz Lipiński <bartosz.s.lipinski@gmail.com>
2023-11-19 17:03:58 +0100

    feat: add firebase

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,16 @@
+import json
+from os import getenv
 from flask import Flask
+from firebase_admin import credentials, initialize_app
 from .routes import init_routes
 
 
 def create_app():
     app = Flask(__name__)
+
+    cert = json.loads(getenv("FIREBASE_SERVICE_ACCOUNT"))
+    cred = credentials.Certificate(cert)
+    initialize_app(cred)
+
     init_routes(app)
     return app
