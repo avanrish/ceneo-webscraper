74bf90fe44d7d71515a7cf3416f39375046ea274
Bartosz Lipiński <bartosz.s.lipinski@gmail.com>
2023-11-26 17:53:51 +0100

    feat: create 404 page and set up routes for products

--- app/web_routes.py
+++ app/web_routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from .services.store import Store
 
 web = Blueprint('web', __name__)
 
@@ -6,3 +7,21 @@ web = Blueprint('web', __name__)
 @web.route('/')
 def home():
     return render_template('home/page.html')
+
+
+@web.route('/products')
+def products():
+    return render_template('products/page.html')
+
+
+@web.route('/products/<product_id>')
+def product(product_id):
+    product = Store.get_product(product_id)
+    if product is None:
+        return render_template('product/not-found.html'), 404
+    return render_template('product/page.html')
+
+
+@web.route('/<path:path>')
+def catch_all(path):
+    return render_template('404.html', hide_navigation=True), 404
